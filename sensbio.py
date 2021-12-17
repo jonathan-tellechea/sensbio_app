ea81a128fffe2e8297edd3e7fd4f10b1c7d1c291
jonathan-tellechea <95962057+jonathan-tellechea@users.noreply.github.com>
2021-12-17 09:20:46 +0100

    update description

--- sensbio.py
+++ sensbio.py
@@ -8,18 +8,13 @@ import pandas as pd
 app = MultiApp()
 
 st.markdown("""
-# Sensbio web application
+# Sensbio: An online server for biosensor design
 
-The application is divided in three interactive pages.
+The application is divided into three interactive pages.
 
-- Data visualization:
-    - Designed to help to better understand the sensbio database.
-- Molecular prediction tool:
-    - The user can find molecules similar to their input and find candidate TF where to start the design of new biosensor circuits specific for the target molecule.
-- Sequence prediction tool
-    - The user can find similar protein sequence to the ones already described in the database and the ligand molecule that triggers them.
-
-**Attribution note:** The multi-page functionality is using the [streamlit-multiapps](https://github.com/upraneelnihar/streamlit-multiapps) framework developed by [Praneel Nihar](https://medium.com/@u.praneel.nihar).
+- **Data visualization:** designed to help to better understand the sensbio tool.
+- **Molecular prediction:** tool: the user can find molecules similar to their input and find candidate TF where to start the design of new biosensor circuits specific for the target molecule.
+- ****Sequence prediction tool:** the user can find similar protein sequences similar to the ones already described in the database and the ligand molecule that triggers them.
 
 """)
 
@@ -30,3 +25,6 @@ app.add_app("Sequence similarity", sequence.app)
 app.add_app("Tutorial", tutorial.app)
 # The main app
 app.run()
+
+st.markdown("""**Attribution note:** The multi-page functionality is using the [streamlit-multiapps](https://github.com/upraneelnihar/streamlit-multiapps) framework developed by [Praneel Nihar](https://medium.com/@u.praneel.nihar).
+""")
