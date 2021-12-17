e1bbed519320d739c2a3d2d2af999815582266fe
jonathan-tellechea <95962057+jonathan-tellechea@users.noreply.github.com>
2021-12-17 14:34:09 +0100

    update files

--- sensbio.py
+++ sensbio.py
@@ -7,6 +7,17 @@ import pandas as pd
 
 app = MultiApp()
 
+st.set_page_config(
+     page_title="Sensbio App",
+     layout="wide",
+    )
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+
 st.markdown("""
 # Sensbio: An online server for biosensor design
 
