6ac776db4ff015862a329a5d42c6545d85c2d75c
morenolopezraul03 <morenolopezraul03@gmail.com>
2022-06-09 09:41:02 +0200

    predictive model implementation

--- apps/molecular.py
+++ apps/molecular.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+import tensorflow as tf
 
 from rdkit import Chem
 from rdkit.Chem import AllChem
@@ -57,21 +59,88 @@ def app():
         fp2 = AllChem.GetMorganFingerprintAsBitVect(mol2, 5, nBits=2048)
         sim = round(DataStructs.TanimotoSimilarity(fp1,fp2),3)
         return sim
+    
+    #define a function to predict affinity between a molecule and a sequence
+    def model_prediction(seq, smi, model):
+        ms = Chem.MolFromSmiles(smi)
+        fp = Chem.RDKFingerprint(ms, fpSize = 512).ToBitString()
+        fp = np.array([int(x) for x in list(fp)])
+        fp = fp.reshape((1,512))
+        
+        aa_list = ["A","C","D","E","F","G","H","I","K","L","M","N","P","Q","R",
+                   "S","T","V","W","Y"]
+        aam = np.zeros((20, 978)).astype(int)
+        pos = 0
+        for aa in seq:
+            if aa == "*" or aa == "X":
+                pos = pos + 1
+                continue
+            aa_idx = aa_list.index(aa)
+            aam[aa_idx][pos] = 1
+            pos = pos + 1
+        aam = aam.reshape((1,20,978))
+            
+        pred_score = model.predict([aam, fp])
+        pred_score = pred_score[0][0]
+        if pred_score < 0:
+            pred_score = 0
+        # elif pred_score > 1:
+        #     pred_score = 1
+            
+        return pred_score
 
     #define a function to rank the molecules by their Tanimoto score against the input molecule.
-    def tanimoto_ranker(query): #query must be a SMILES string
+    def tanimoto_ranker(query, model): #query must be a SMILES string
         df_mol = pd.read_csv('./TF_DB_clean_pathway.csv')
         simil = []
         for i in df_mol['SMILES']:
             simil.append(float(tanimoto_calc(query,i)))
         df_mol['Tanimoto_score_vs_query'] = simil
+        
+        if flag:
+            affin = []
+            for j in df_mol['AA_sequence']:
+                affin.append(model_prediction(j,query,model))
+        else:
+            idx_simil = sorted(range(len(simil)), key=lambda k: simil[k], reverse=True)[0:num_check]
+            affin = [None] * len(simil)
+            for j in idx_simil:
+                affin[j] = model_prediction(df_mol['AA_sequence'][j],query,model)
+            
+        df_mol['Affinity prediction'] = affin
+        
         df_mol = df_mol.sort_values(by=['Tanimoto_score_vs_query'], ascending=False).reset_index(drop=True)
         return df_mol
+    
+    st.write("Use this checkbox to decide if predictions are performed over all" 
+             " the database. Otherwise, they will be performed only on the"
+             " first 100/500/1000.")
+    
+    check = st.radio("Select an option:",
+                     ("Perform predictions over all the database",
+                      "Perform predictions over the first 100",
+                      "Perform predictions over the first 500",
+                      "Perform predictions over the first 1000"))
+    
+    if check == "Perform predictions over all the database":
+        flag = True
+    elif check == "Perform predictions over the first 100":
+        flag = False
+        num_check = 100
+    elif check == "Perform predictions over the first 500":
+        flag = False
+        num_check = 500
+    elif check == "Perform predictions over the first 1000":
+        flag = False
+        num_check = 1000
+    
 
     # user input text boxes for SMILES
     user_input_mol = st.text_input("Paste molecule in SMILES format")
     if user_input_mol != "" and chem_validator(user_input_mol):
-        df = tanimoto_ranker(user_input_mol)
+        # loads predictive model
+        pred_model = tf.keras.models.load_model('./final_model')
+        df = tanimoto_ranker(user_input_mol, pred_model)
         df_head = df.head(100)
         st.write(df_head)
 
